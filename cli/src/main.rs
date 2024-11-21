8d92944c7eaa0549065b2c57271908f12a422dbd
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-11-21 16:13:51 +0100

    feat: add tracing to cli
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,48 +5,61 @@ use bluer::{Adapter, AdapterEvent, Address, DiscoveryFilter, DiscoveryTransport}
 use bluer_miflora::Miflora;
 use futures::{pin_mut, StreamExt};
 
-// async fn query_all_device_properties(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
-//     let device = adapter.device(addr)?;
-//     let props = device.all_properties().await?;
-//     for prop in props {
-//         println!("    {:?}", &prop);
-//     }
-//     Ok(())
-// }
+fn enable_tracing() {
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::EnvFilter;
+
+    if tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "miflora=debug".into()))
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()
+        .is_err()
+    {
+        tracing::warn!("tracing already set");
+    }
+}
 
 pub async fn handle(adapter: Adapter, addr: Address) -> anyhow::Result<()> {
     let miflora = Miflora::from_adapter(&adapter, addr)?;
     miflora.try_connect(5).await?;
-    println!("info:   {:?}", miflora.read_system().await?);
-    println!("values: {:?}", miflora.read_historical_values().await?);
+    let system = miflora.read_system().await?;
+    tracing::info!(message = "system information", address = %addr, battery = system.battery(), firmware = %system.firmware());
+    let values = miflora.read_realtime_values().await?;
+    tracing::info!(
+        message = "realtime values",
+        address = %addr,
+        temperature = values.temperature(),
+        brightness = values.brightness(),
+        moisture = values.moisture(),
+        conductivity = values.conductivity(),
+    );
     miflora.try_disconnect(5).await?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    enable_tracing();
+
     let addresses: HashSet<_> = env::args()
         .filter_map(|arg| arg.parse::<Address>().ok())
         .collect();
 
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
-    println!(
-        "Discovering devices using Bluetooth adapter {}\n",
+    tracing::info!(
+        "discovering devices using Bluetooth adapter {}",
         adapter.name()
     );
     adapter.set_powered(true).await?;
 
-    let filter = DiscoveryFilter {
-        transport: DiscoveryTransport::Le,
-        pattern: Some("Flower care".into()),
-        ..Default::default()
-    };
-    adapter.set_discovery_filter(filter).await?;
-    println!(
-        "Using discovery filter:\n{:#?}\n\n",
-        adapter.discovery_filter().await
-    );
+    adapter
+        .set_discovery_filter(DiscoveryFilter {
+            transport: DiscoveryTransport::Le,
+            pattern: Some("Flower care".into()),
+            ..Default::default()
+        })
+        .await?;
 
     let device_events = adapter.discover_devices().await?;
     pin_mut!(device_events);
@@ -56,15 +69,15 @@ async fn main() -> anyhow::Result<()> {
             AdapterEvent::DeviceAdded(addr) => {
                 let device = adapter.device(addr)?;
                 let name = device.name().await?;
-                println!("device {addr} discovered {name:?}");
+                tracing::debug!(message = "device discovered", address = %addr, name = ?name);
                 if addresses.contains(&addr) {
                     if let Err(err) = handle(adapter.clone(), addr).await {
-                        println!("=> something wend wrong with {addr}: {err:?}");
+                        tracing::warn!(message = "something went wrong", address = %addr, error = %err);
                     }
                 }
             }
             AdapterEvent::DeviceRemoved(addr) => {
-                println!("device {addr} disappeared");
+                tracing::debug!(message = "device disappeared", address = %addr);
             }
             _ => {}
         }
